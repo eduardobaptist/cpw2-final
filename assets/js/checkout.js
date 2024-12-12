9f843b58ecd3cbccaaa762fd501c3f89c2560789
eduardo baptista <aristimdudu@gmail.com>
2024-12-12 16:05:10 -0300

    ajax desnecessário removido

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -11,19 +11,6 @@ $(document).ready(() => {
 
   $("#user").append(userEmail);
 
-  $.ajax({
-    url: `https://dummyjson.com/products/${id}`,
-    method: "GET",
-    success: (product) => {},
-    error: (xhr, status, error) => {
-      $("main").html(
-        `<div class="w-100 d-flex justify-content-center">
-            <p class="text-danger mt-3">Erro ao carregar dados: ${error}</p>
-          </div>`
-      );
-    },
-  });
-
   //steps
   $(".next-step").click(function () {
     const modoPagamento = $('input[name="payment"]:checked').val();
